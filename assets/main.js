2893a6e6038cd54d220d4860efd8c5dcb834e8f0
AryaK <ecstaticlimb@gmail.com>
2023-05-05 14:08:24 -0700

    Changes for co2 API implemented

--- assets/main.js
+++ assets/main.js
@@ -16,7 +16,22 @@ function callAirQualAPI(){
 }
 
 function callCO2API(){
+    const settings = {
+        async: true,
+        crossDomain: true,
+        url: 'https://daily-atmosphere-carbon-dioxide-concentration.p.rapidapi.com/api/co2-api',
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'ee13e75d97mshaf4fdae5fe7ab48p1cf91ajsn89594c589298',
+            'X-RapidAPI-Host': 'daily-atmosphere-carbon-dioxide-concentration.p.rapidapi.com'
+        }
+    };
 
+    let co2Data;
+    $.ajax(settings).done(function (response) {
+        co2Data = response;
+        console.log(co2Data);
+    });
 }
 
 function updateOutput(electricityResponse, airQualityResponse, co2Response){
