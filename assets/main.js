9486a46c2d190ba14f9f296a0b4d60c3d475d75d
AryaK <ecstaticlimb@gmail.com>
2023-05-05 15:04:12 -0700

    added new method for carbon dioxide return

--- assets/main.js
+++ assets/main.js
@@ -2,6 +2,9 @@ function searchButtonScript() {
     electricityResponse = callElectricityAPI();
     airQualityResponse = callAirQualAPI();
     co2Response = callCO2API();
+    //remove if want 
+    //the str is the string city from the search bar
+    airPollution = callAirPollAPI(str);
 
     updateOutput(electricityResponse,airQualityResponse,co2Response);
 }
@@ -20,6 +23,7 @@ function callCO2API(){
         async: true,
         crossDomain: true,
         url: 'https://daily-atmosphere-carbon-dioxide-concentration.p.rapidapi.com/api/co2-api',
+        //this gives general world data for co2 emmisions 
         method: 'GET',
         headers: {
             'X-RapidAPI-Key': 'ee13e75d97mshaf4fdae5fe7ab48p1cf91ajsn89594c589298',
@@ -33,6 +37,26 @@ function callCO2API(){
         console.log(co2Data);
     });
     return co2Data;
+}
+
+function callAirPollAPI(str) {
+    const settings = {
+        async: true,
+        crossDomain: true,
+        //location in url is based upon parameter of method, which is given as the city in searchbar
+        url: 'https://air-quality-by-api-ninjas.p.rapidapi.com/v1/airquality?city=${str}',
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'ee13e75d97mshaf4fdae5fe7ab48p1cf91ajsn89594c589298',
+            'X-RapidAPI-Host': 'air-quality-by-api-ninjas.p.rapidapi.com'
+        }
+    };
+    let polData;
+    $.ajax(settings).done(function (response) {
+        polData = response;
+        console.log(polData);
+    });
+    return polData;
 }
 
 function updateOutput(electricityResponse, airQualityResponse, co2Response){
