46bc7602efcbbf84842c9b532107a78a5d35c136
Vladimir Kupryukhin <vladimirkupryukhin@gmail.com>
2023-05-05 13:12:40 -0700

    Basic JS setup

---
+++ assets/main.js
@@ -0,0 +1,24 @@
+function searchButtonScript() {
+    electricityResponse = callElectricityAPI();
+    airQualityResponse = callAirQualAPI();
+    co2Response = callCO2API();
+
+    updateOutput(electricityResponse,airQualityResponse,co2Response);
+}
+
+
+function callElectricityAPI(){
+
+}
+
+function callAirQualAPI(){
+
+}
+
+function callCO2API(){
+
+}
+
+function updateOutput(electricityResponse, airQualityResponse, co2Response){
+
+}
