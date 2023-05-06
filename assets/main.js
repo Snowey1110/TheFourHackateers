77f47b07f377759651290bb9efdf68932f17cda6
agrach060 <anna.gracheva060@gmail.com>
2023-05-05 18:15:11 -0700

    Implemented callAirQuality function

--- assets/main.js
+++ assets/main.js
@@ -3,22 +3,28 @@ function searchButtonScript() {
     airQualityResponse = callAirQualAPI();
     co2Response = callCO2API();
 
-    updateOutput(electricityResponse,airQualityResponse,co2Response);
+    updateOutput(electricityResponse, airQualityResponse, co2Response);
 }
 
 
-function callElectricityAPI(){
+function callElectricityAPI() {
 
 }
 
-function callAirQualAPI(){
+function callAirQualAPI() {
+    const API_KEY = '61a0cafd-c4c5-47c1-8250-b2ca702301b2';
+    const url = `https://api.example.com/airquality?key=${API_KEY}`;
+
+    return fetch(url)
+        .then(response => response.json())
+        .then(data => data.results)
+        .catch(error => console.error(error));
+}
+
+function callCO2API() {
 
 }
 
-function callCO2API(){
-
-}
-
-function updateOutput(electricityResponse, airQualityResponse, co2Response){
+function updateOutput(electricityResponse, airQualityResponse, co2Response) {
 
 }
