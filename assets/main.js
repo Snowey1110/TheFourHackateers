986879deda7c699ed6c3ca49722fab1b8bf21e5d
agrach060 <anna.gracheva060@gmail.com>
2023-05-06 15:25:43 -0700

    Estimated AirQuality level

--- assets/main.js
+++ assets/main.js
@@ -15,20 +15,50 @@ function callAirQualAPI() {
     return fetch('http://api.airvisual.com/v2/city?city=Los%20Angeles&state=California&country=USA&key=61a0cafd-c4c5-47c1-8250-b2ca702301b2')
         .then(response => response.json())
         .then(data => {
-            return data; // Return the parsed data
+            const cityData = {
+                "pollution": [
+                    {
+                        "ts": data.data.current.pollution.ts,
+                        "aqius": data.data.current.pollution.aqius,
+                        "mainus": data.data.current.pollution.mainus,
+                        "aqicn": data.data.current.pollution.aqicn,
+                        "maincn": data.data.current.pollution.maincn,
+                        [data.data.current.pollution.mainus]: {
+                            "conc": data.data.current.pollution[data.data.current.pollution.mainus]?.conc,
+                            "aqius": data.data.current.pollution[data.data.current.pollution.mainus]?.aqius,
+                            "aqicn": data.data.current.pollution[data.data.current.pollution.mainus]?.aqicn
+                        }
+                    }
+                ]
+            };
+            console.log(cityData);
+            console.log(checkPollutionLevel(cityData));
         })
         .catch(error => {
             console.error('Error:', error);
         });
 }
-callAirQualAPI().then(data => {
-    // Use the parsed data here
-    console.log(data);
-}).catch(error => {
-    // Handle any errors that occurred during the request
-    console.error('Error:', error);
-});
 
+function checkPollutionLevel(cityData) {
+    const aqiUS = cityData.pollution[0].aqius;
+    const aqiCN = cityData.pollution[0].aqicn;
+
+    if (aqiUS <= 50 && aqiCN <= 50) {
+        return "The air quality in this city is good";
+    } else if (aqiUS <= 100 && aqiCN <= 100) {
+        return "The air quality in this city is moderate";
+    } else if (aqiUS <= 150 && aqiCN <= 150) {
+        return "The air quality in this city is unhealthy for sensitive groups";
+    } else if (aqiUS <= 200 && aqiCN <= 200) {
+        return "The air quality in this city is unhealthy";
+    } else if (aqiUS <= 300 && aqiCN <= 300) {
+        return "The air quality in this city is very unhealthy";
+    } else {
+        return "The air quality in this city is hazardous";
+    }
+}
+
+callAirQualAPI();
 function callCO2API() {
 
 }
