46447def299b8236594d73891788d9f41f071f85
AryaK <ecstaticlimb@gmail.com>
2023-05-07 01:48:14 -0700

    added co2 API/confirmed it works

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,7 @@
 function searchButtonScript() {
     electricityResponse = callElectricityAPI();
     airQualityResponse = callAirQualAPI();
-    co2Response = callCO2API();
-    //remove if want 
-    //the str is the string city from the search bar
-    airPollution = callAirPollAPI(str);
-    directionTime = callDirecAPI(str1, str2);
-
+    co2Response = callCO2API("Seattle");
     updateOutput(electricityResponse,airQualityResponse,co2Response);
 }
 
@@ -19,28 +14,8 @@ function callAirQualAPI(){
 
 }
 
-function callCO2API(){
-    const settings = {
-        async: true,
-        crossDomain: true,
-        url: 'https://daily-atmosphere-carbon-dioxide-concentration.p.rapidapi.com/api/co2-api',
-        //this gives general world data for co2 emmisions 
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'ee13e75d97mshaf4fdae5fe7ab48p1cf91ajsn89594c589298',
-            'X-RapidAPI-Host': 'daily-atmosphere-carbon-dioxide-concentration.p.rapidapi.com'
-        }
-    };
 
-    let co2Data;
-    $.ajax(settings).done(function (response) {
-        co2Data = response;
-        console.log(co2Data);
-    });
-    return co2Data;
-}
-
-function callAirPollAPI(loc) {
+function callCO2API(loc){
     const settings = {
         async: true,
         crossDomain: true,
@@ -58,36 +33,6 @@ function callAirPollAPI(loc) {
         console.log(polData);
     });
     return polData;
-}
-
-function callDirecAPI(start, end) {
-    const apiKey = "AIzaSyB1csRyD8kzD2z-wvrhLQM41QRCm4KozNg";
-const origin = start;
-const destination = end;
-
-// Define the API endpoint and parameters
-const url = `https://maps.googleapis.com/maps/api/directions/json?origin=${encodeURIComponent(origin)}&destination=${encodeURIComponent(destination)}&key=${apiKey}`;
-
-// Send the request and get the response
-fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    // Extract the distance and duration from the response
-    const distance = data.routes[0].legs[0].distance.text;
-    const duration = data.routes[0].legs[0].duration.text;
-    
-    // Print the results
-    console.log(`Driving distance from ${origin} to ${destination}: ${distance}`);
-    console.log(`Driving time from ${origin} to ${destination}: ${duration}`);
-  })
-  .catch(error => {
-    console.error(error);
-  });
-  let jsonObject = {
-    distanceResult: `Driving distance from ${origin} to ${destination}: ${distance}`,
-    drivingTime: `Driving time from ${origin} to ${destination}: ${duration}`,
-  }
-  return jsonObject;
 }
 
 function updateOutput(electricityResponse, airQualityResponse, co2Response){
